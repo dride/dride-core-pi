cecb2d80e3d481157f1bf49e5552c9b5ff1b8aee
Yossi <saoron@gmai.com>
2016-05-27 14:53:22 +0300

    add some tests

--- cardiganTest.py
+++ cardiganTest.py
@@ -1,4 +1,23 @@
+import random
+import unittest
+import sys
+
+sys.path.insert(0,'/usr/lib/pyshared/python2.7')
+
 import cv2
+import numpy as np
+
+from cvprocessor import hasNoBlack
+
+"""Simple test harness"""
+class TestImages(unittest.TestCase):
+
+    def test_white(self):
+        self.assertTrue(hasNoBlack(cv2.imread("test_images/white.png")))
+
+    def test_black(self):
+        self.assertFalse(hasNoBlack(cv2.imread("test_images/black.png")))
 
 
-print cv2.__version__
+if __name__ == '__main__':
+    unittest.main()
