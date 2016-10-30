4ef1f5dbe1cc792be496a651749c8da29a93ad21
Yossi <saoron@gmail.com>
2016-10-30 16:21:56 +0200

    cleanup filename on ws

--- dride-ws/server/api/getClips/getClips.controller.js
+++ dride-ws/server/api/getClips/getClips.controller.js
@@ -11,11 +11,25 @@ var fileNames   = [];
 // Get list of getPOLists
 exports.index = function(req, res) {
 	//const videoClipsFolder = config.videoClipsFolder;
-	const videoClipsFolder = '../modules/video/clip/';
+
+	const videoClipsFolder = '../../modules/video/clip/';
 	const fs = require('fs');
 
+	var files = fs.readdirSync(videoClipsFolder);
+	for (var i in files) {
+
+	  console.log('Model Loaded: ' + files[i]);
+	  if (files[i] == '.DS_Store' || files[i] == '.gitignore')
+	  	continue;
+
+	  files[i] = (files[i].split('.'))[0]
+
+	  fileNames.push(files[i]);
+	}
+
+
     res.json({data: 
-    				fs.readdirSync(videoClipsFolder)
+    				fileNames
     		});
 
 
