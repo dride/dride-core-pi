ab1a4326818ae584a5012262ffa3d18ee9237f0b
Yossi <saoron@gmail.com>
2018-03-13 18:28:38 +0200

    close #65 #62

--- modules/video/index.js
+++ modules/video/index.js
@@ -1,13 +1,17 @@
+var record = require('./helpers/record');
+var settingsHelper = require('../settings/settings');
+var verifyCamera = require('./helpers/verifyCamera');
+var spawn = require('child_process').spawn;
+var fs = require('fs');
 
-var record = require('./helpers/record')
-var spawn = require("child_process").spawn;
-
-var interval = 60*1000
-
-record.recordClip(new Date().getTime(), interval);
-
-setInterval(_=>{
-	console.log('start..')
-	record.recordClip(new Date().getTime(), interval);
-}, interval + 1500)
- 
+var settings = settingsHelper.getSettings();
+var interval = settings.clipLength * 30 * 1000;
+console.log(interval);
+if (settings.videoRecord) {
+	//make sure we have the camera connected, If not notify with an error LED,
+	if (!verifyCamera.verifyCamera()) {
+		spawn('python', ['/home/Cardigan/modules/indicators/python/states/standalone.py', 'error']);
+	} else {
+		record.recordClip(new Date().getTime(), interval);
+	}
+}
