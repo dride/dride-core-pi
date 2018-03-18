1e69a20b95f10c91628d9f562a35185c0e637c4f
Yossi <saoron@gmail.com>
2018-03-18 14:02:56 +0200

    test(video): test RaspiCam init

---
+++ modules/video/test/video.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+const expect = require('chai').expect;
+var RaspiCam = require('raspicam');
+
+describe('Video', () => {
+	it('should init RaspiCam class', () => {
+		expect(true).to.not.be.null;
+	});
+});
