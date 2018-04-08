02ac53b3f95647a1822dcdecd91ffa6b331d6cad
Yossi <saoron@gmail.com>
2018-04-08 17:34:14 +0300

    fix wrong param in setSettings dvr

--- dride-ws/server/api/setSetting/setSetting.controller.js
+++ dride-ws/server/api/setSetting/setSetting.controller.js
@@ -2,39 +2,39 @@
 
 var path = require('path');
 var fs = require('fs');
-var exec = require('child_process');
+var exec = require('child_process').exec;
 
-exports.index = function(req, res) {
-	var fieldName = req.param('fieldName');
-	var fieldValue = req.param('fieldValue');
+exports.index = function (req, res) {
+  var fieldName = req.param('fieldName');
+  var fieldValue = req.param('fieldValue');
 
-	var defaults = path.join(__dirname, '../../../..', 'config.json');
-	var config = JSON.parse(fs.readFileSync(defaults, 'utf-8'));
+  var defaults = path.join(__dirname, '../../../..', 'config.json');
+  var config = JSON.parse(fs.readFileSync(defaults, 'utf-8'));
 
-	fieldValue = fieldValue.toString().toLowerCase();
+  fieldValue = fieldValue.toString().toLowerCase();
 
-	//cast to boolean if needed
-	if (fieldValue == 'true') fieldValue = true;
-	if (fieldValue == 'false') fieldValue = false;
+  //cast to boolean if needed
+  if (fieldValue == 'true') fieldValue = true;
+  if (fieldValue == 'false') fieldValue = false;
 
-	config['settings'][fieldName] = fieldValue;
+  config['settings'][fieldName] = fieldValue;
 
-	try {
-		fs.writeFileSync(defaults, JSON.stringify(config));
+  try {
+    fs.writeFileSync(defaults, JSON.stringify(config));
 
-		//if fieldName is DVR enable/disbale record.service
-		if (fieldName == 'dvr') {
-			exec('sudo systemctl ' + (fieldValue ? 'start' : 'stop') + ' record');
-		}
+    //if fieldName is DVR enable/disbale record.service
+    if (fieldName == 'videoRecord') {
+      exec('sudo systemctl ' + (fieldValue ? 'start' : 'stop') + ' record');
+    }
 
-		res.json({
-			status: '1',
-			reason: 'setting was saved successfully!'
-		});
-	} catch (err) {
-		res.json({
-			status: '0',
-			reason: err
-		});
-	}
+    res.json({
+      status: '1',
+      reason: 'setting was saved successfully!'
+    });
+  } catch (err) {
+    res.json({
+      status: '0',
+      reason: err
+    });
+  }
 };
